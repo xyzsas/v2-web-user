161d3bd44c313b5ee11197c5f3765e01a89444ff
Phantomlsh <phantomlsh@outlook.com>
2021-04-03 16:06:00 +0800

    Update Center.vue

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -1,27 +1,30 @@
-<!-- Page for user center
-1. 修改信息
-2. 引导用户上传图片（1寸照片大小）
-3. 绑定QQ -->
 <template>
-  <div class="container content" v-if="SS.token">
-    <h1>个人中心</h1>
-    <p>用户组：<code>{{ SS.group }}</code></p>
-    <figure style="width: 160px; height: 240px; padding: 0px; margin: 5px 0px 5px 0px; border: 3px #efefef solid">
-      <img :src="img" alt="个人照片" width="160" height="240">
-    </figure>
-    <div>
-      <tr class="control" v-for="(v, k) in data">
-        <td>
-          <p>{{ map[k] }}</p>
-        </td>
-        <td>
-          <input v-model="data[k]" class="input" type="text" :placeholder="v" style="max-width: 400px">
-        </td>
-      </tr>
+  <div class="center">
+    <div class="box" style="width: 360px;">
+      <h1 class="title">个人中心</h1>
+      <p class="subtitle is-6 mt-3" v-if="loading">正在载入...</p>
+      <template v-else>
+        <div class="is-flex is-justify-content-space-between">
+          <div class="p-3">
+            <code class="is-inline-block">id: {{ SS.id }}</code>
+            <p class="mt-2">姓名：<b>{{ SS.name }}</b></p>
+            <p class="mt-2">角色：<b>{{ SS.role }}</b></p>
+            <p class="mt-2">用户组：<br v-if="SS.group.length > 8"><code>{{ SS.group }}</code></p>
+          </div>
+          <img :src="img" onerror="this.src = '/img/placeholder.png'" alt="照片" width="130" height="182" @click="router.push('/photo')">
+        </div>
+        <div class="mt-6 form">
+          <div v-for="(v, k) in profile" class="p-2">
+            <label>{{ k }}</label>
+            <input type="text" class="input is-small" v-model="profile[k]" placeholder="请填写">
+          </div>
+          <div class="p-2">
+            <button class="button is-primary is-fullwidth mt-3" :class="{ 'is-loading': submitLoading }" @click=
+            "submit">提交</button>
+          </div>
+        </div>
+      </template>
     </div>
-    <button class="button is-primary" @click="submit">
-      <span>提交修改</span>
-    </button>
   </div>
 </template>
 
@@ -32,57 +35,68 @@ import axios from '../plugins/axios'
 
 const router = useRouter()
 const SS = window.sessionStorage
-const location = window.location
-const map = {
-  name: '姓名：',
-  id: '身份证号：',
-  address: '家庭住址：',
-  father: '父亲姓名：',
-  mother: '母亲姓名：',
-  phone: '电话号码：'
-}
+if (!SS.token) router.push('/login')
+const opt = { headers: { token: SS.token } }
 
-ref: msg = []
 ref: loading = true
+ref: submitLoading = false
 ref: img = ''
-ref: data = {
-  name: 'abc',
-  id: '1234345',
-  address: '12xwd',
-  father: 'fff',
-  mother: 'mmm',
-  phone: '222'
+ref: profile = {
+  '身份证': '',
+  '家庭住址': '',
+  '父亲姓名': '',
+  '父亲联系方式': '',
+  '母亲姓名': '',
+  '母亲联系方式': '',
+  '毕业学校': ''
 }
 
-// if (!SS.token) router.push('/login')
-// else axios // get messages
-//   .get('/msg', { headers: { token: SS.token } })
-//   .then(({ data }) => {
-//     loading = false
-//     msg = data.map(x => x.split('$$'))
-//   })
-//   .catch(err => {
-//     Swal.fire({
-//       text: err.response ? err.response.data : '网络错误',
-//       icon: 'error',
-//       willClose: () => { router.push('/login') }
-//     })
-//   })
+async function catchErr (e, jmp = true) {
+  console.log(e)
+  await Swal.fire('错误', e.response ? e.response.data : e.toString(), 'error')
+  if (jmp) router.push('/')
+  return false
+}
 
-const submit = (() => {
-  console.log('submit')
-})
+function getUrl (type) {
+  return axios.get('/store?type=' + type, opt)
+    .then(({ data }) => data)
+    .catch(catchErr)
+}
+
+async function fetch () {
+  img = await getUrl('photo')
+  const pUrl = await getUrl('profile')
+  try {
+    const res = await axios.get(pUrl)
+    for (const k in profile) {
+      if (res.data[k]) profile[k] = res.data[k]
+    }
+  } catch {}
+  loading = false
+}
+fetch()
+
+async function submit () {
+  submitLoading = true
+  try {
+    const res = await axios.post('/store', { type: 'profile' }, opt)
+    await axios.put(res.data, profile)
+    Swal.fire('提交成功', '', 'success')
+  } catch (e) {
+    catchErr(e, false)
+  }
+  submitLoading = false
+}
 </script>
 
 <style scoped>
-div.container {
+div.center {
+  min-height: 100vh;
+  padding: 20px 0;
   width: 100%;
-  max-width: 500px;
-  height: 100vh;
-  padding: 24px;
-}
-td {
-  vertical-align: middle;
-  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
