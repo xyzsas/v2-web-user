f007f9768b33dabec5d6d67d7f2e46622fc1da12
Layzzi <Shadow_sock@icloud.com>
2021-05-10 19:57:47 +0800

    update fox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,7 @@
       </div>
     </div>
   </div>
-  <img src="../../public/img/xyzsas.png" style="position: absolute; right: 0; bottom: -50px;">
+  <img src="/img/fox.png" style="position: absolute; right: 0; bottom: -50px;">
 </template>
 
 <script setup>
