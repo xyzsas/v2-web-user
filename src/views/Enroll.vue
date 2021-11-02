e09ba0c95bc30a116fb331ca92056c2525cd06e3
Layzzi <Shadow_sock@icloud.com>
2021-11-02 13:21:10 +0800

    Update Enroll.vue

--- src/views/Enroll.vue
+++ src/views/Enroll.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="title is-3">社团招新</div>
-    <div class="note mb-3">本次社团招新为XYZSAS v3技术实验，请按照指示操作。<strong>请勿反复刷新!</strong></div>
+    <div class="note mb-3"><strong>v3系统将不能集体重置密码，此后忘记学生事务系统密码须自行承担延误抢课与查分等功能的风险！</strong></div>
     <div v-if="clock" class="mt-6">
       <h1 class="title is-3" style="text-align: center;">社团招新尚未开始<br><code class="m-2">{{ clock }}</code></h1>
       <p style="text-align: center;">倒计时结束后系统会自动载入数据</p>
@@ -15,10 +15,10 @@
         </tr>
       </thead>
       <tfoot>
-        <tr v-for="c in all">
+        <tr v-for="(c, i) in all">
           <th>{{ c.name }}</th>
           <th>{{ c.left }}</th>
-          <th v-if="all.indexOf(c) != selected"><button class="button is-small is-info" :disabled="selected || c.left <= 0" :class="{ 'is-loading': loading, 'is-danger': c.left <= 0 }" @click="submit(all.indexOf(c))">{{ c.left > 0 ? '加入' : '已满' }}</button></th>
+          <th v-if="i != selected"><button class="button is-small is-info" :disabled="selected || c.left <= 0" :class="{ 'is-loading': loading, 'is-danger': c.left <= 0 }" @click="submit(c, i)">{{ c.left > 0 ? '加入' : '已满' }}</button></th>
           <th v-else><button class="button is-small is-primary" disabled :class="{ 'is-loading': loading }">已加入</button></th>
         </tr>
       </tfoot>
@@ -30,8 +30,9 @@
 <script setup>
 import { U, token } from '../plugins/state.js'
 import { useRouter } from 'vue-router'
+import axios from '../plugins/axios'
 const router = useRouter()
-const start = new Date("2021-11-04T12:20:00.000+08:00").getTime()
+const start = new Date("2021-11-02T12:20:00.000+08:00").getTime()
 
 if (!U.value) router.push('/')
 
@@ -42,7 +43,6 @@ async function catchErr (e, jmp = true) {
   return false
 }
 
-const url = 'https://sas.aauth.link/xyz'
 let all = $ref([])
 let selected = $ref(null)
 let loading = $ref(false)
@@ -52,8 +52,8 @@ let clock = $ref('Loading')
 async function getAll() {
   pageLoading = true
   all = []
-  const findModel = { v: 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJcIixcIl9cIjpcIj9cIn0se1wiI1wiOlwiY2x1YmRhdGFcIixcIl9cIjpcIj9cIixcIjpcIjp7XCIke2lkfVwiOjF9fV0iLCI_IjpbImlkIl0sIm5iZiI6MTYzNTk5OTYwMDAwMH0.VsKedpKnSKRbfGIslUj4SHZYEjK253R7LxSR1cCcUJxcLJIVuRaihTCjYghy8VNRFn6jSIkTfUi3RJa6H6xHhEd_F_V7-staEWHAmFEkUWcOl9xa8w4gUl9JSgz8mrN6e3-FoVsHj8khqet5dNsPJt3gG6ZQ38hg1MiBUbEuDaOGzVrYwDXObl55eTEH9yDglV5Xycy2XIRGKK_8_kaJRj-HMX0XD83HPk6rjnnjsNGEbpd5nJadNf9X0LuYRlGojIwEngZJnrYm2vC08v2tnnJbFY3qP1GoDxgBVO3QrSSH--PKTvG2blCv9WLEIMpcjTBKMG23vymJy4mrE5sddA', s: { id: U.value.id } }
-  await axios.post(url, findModel, token())
+  const findModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJcIixcIl9cIjpcIj9cIn0se1wiI1wiOlwiY2x1YmRhdGFcIixcIl9cIjpcIj9cIixcIjpcIjp7XCIke2lkfVwiOjF9fV0iLCI_IjpbImlkIl19.RAVszZa5qAaYdGQMzeQ0yLdmVdf7bVa_75k1wcYkGqAYoUp-qXhkhYz2lV8IqUs53rA1LPTPgnozDd9HwDI2zW6USVwV-qIparrcDk0hI2HsBN8vWpW0Ys1VBuYhHPTwhsyy0KQV-QWOfMarRVU1IXNH979dq0oXsefTmUSjDbNoKKOjjOJGYPwDbmhn4sZ2j069hqhgxlLIsMBBCE9azOLaTpayghRfKpY8p15E4YzmXP2Gcow28AIG2qnOof9komwVVHYm0sJf4CjkDvq4SbKU8tzob7Tj53_-uCz3wpv6eiOYDEKgtlrycUZ62gM2CzSoNZFC2pwXxXPKsIrPOg', s: { id: U.value.id } }
+  await axios.post('/xyz', findModel, token())
     .then(({data}) => {
       if (!data[0].ok) Swal.fire('错误', '请重新进入', 'error')
       const cs = data[0].result.options.split(',')
@@ -69,15 +69,14 @@ async function getAll() {
   pageLoading = false
 }
 
-async function submit (i) {
+async function submit (c, i) {
   if (selected) return
   loading = true
   let cancel = false
-  const submitModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJkYXRhXCIsXCJfXCI6XCI_XCIsXCIhXCI6MCxcIjpcIjp7XCIke2lkfVwiOjF9fSx7XCIjXCI6XCJjbHViXCIsXCI6XCI6e1wiJCR7aX1cIjp7XCJfXCI6XCJERUNcIn19fSx7XCIjXCI6XCJjbHViZGF0YVwiLFwiOlwiOntcIiR7aWR9XCI6XCIke2l9XCJ9fV0iLCI_IjpbImlkIiwiaSJdLCJuYmYiOjE2MzU5OTk2MDAwMDB9.nqIA_Zd9eFXj1_5GMpyz49-Zr2mMY3up9IpfKukYWez97aM5u09C2QwbY9Ek0m5ghLqIsITzb-Og2IF_qOuyc1uzMMDLnPoFbHkc3LO6E4BcbD7ABqQrLI0fDPDLFnVnaAl1w-QlNl019maoBVzv0bVr44zeq4U-3nDAJ_luanjR7Nuu86OZyJf4kQ7GExoooywpO3rWzwqYj4TSM9vT7NQBT0uQqYTUbzRMTaSH1IrNsKyUToYrKyU3Zt2wp_nYz6x5O0pWVRwqBcG9yIFKV0H5CRZeShRWA9X6hGmfyQ64Y-wElwgvwlDZuMdBVFytPCRz0M8L_9aqNcFKvJe3BQ', s: { id: U.value.id, i: String(i) } }
-  console.log(submitModel)
+  const submitModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJkYXRhXCIsXCJfXCI6XCI_XCIsXCIhXCI6MCxcIjpcIjp7XCIke2lkfVwiOjF9fSx7XCIjXCI6XCJjbHViXCIsXCI6XCI6e1wiJCR7aX1cIjp7XCJfXCI6XCJERUNcIn19fSx7XCIjXCI6XCJjbHViZGF0YVwiLFwiOlwiOntcIiR7aWR9XCI6XCIke2l9XCJ9fV0iLCI_IjpbImlkIiwiaSJdfQ.xZRTGjCEuOO9AUHaAEq72WDrj1WT9y_jDBCoi_yxxiCBpqkRvuS-epr18Pkvx1YkQA_haq26hQ9nHix4AyOu2WySDezbGmXVBSLd9YgPNLiuIG2_AatjONPZwsVR1IhfqFFZsy3ETWoHKTRBgaETQVH8g2WpA-cEQgWUWXiOj-PvHgWT_-xhGugyxeBTHk0-LIthlVk_KqTKEjAcPImezO41et5h6HUFWmOq4pUiOThLs3P8cj8b2AK-zLgImyOt4ZxQ36nOhZw9AZRXREFEAnbp_DjfU1ctUSyMxh6hIz0S2DWkROrU5bi67sguz6PaQpZYzH4nlTMk5CJoA2kVuw', s: { id: U.value.id, i: String(i) } }
   await Swal.fire({
     title: '确认加入吗？',
-    text: '您即将加入' + all[i].name,
+    text: '您即将加入' + c.name,
     icon: 'warning',
     showCancelButton: true,
     confirmButtonText: '确认',
@@ -88,9 +87,8 @@ async function submit (i) {
     })
     .catch(catchErr)
   if (!cancel) {
-    await axios.post(url, submitModel, token())
+    await axios.post('/xyz', submitModel, token())
     .then(({data}) => {
-      console.log(data)
       getAll()
       if (!data[0].ok) {
         Swal.fire('错误', '请勿重复加入社团!', 'error')
@@ -98,28 +96,15 @@ async function submit (i) {
       }
       if (!data[1].ok) {
         Swal.fire('失败', '加入失败, 请查看剩余名额是否充足', 'error')
-        getAll()
         return
       }
       if (!data[2].ok) {
         Swal.fire('失败', '系统错误, 请务必联系开发者QQ:1323933782!', 'error')
         return
       }
-      Swal.fire({
-        title: '加入社团成功! ',
-        text: '诚邀您填写学生事务系统体验问卷',
-        icon: 'success',
-        showCancelButton: true,
-        confirmButtonText: '填写问卷',
-        cancelButtonText: '离开'
-      })
-        .then((res) => {
-          if (res.isConfirmed) router.push('/@/uNhh57K78w')
-          else router.push('/')
-        })
-        .catch(catchErr)
     })
     .catch(catchErr)
+    Swal.fire(`您已加入${c.name}`, '', 'success')
   }
   loading = false
 }
