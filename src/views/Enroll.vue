c452c051cea3cc65037b31f050b31ce18d6505fa
Layzzi <Shadow_sock@icloud.com>
2021-11-02 22:10:52 +0800

    Update Enroll.vue

--- src/views/Enroll.vue
+++ src/views/Enroll.vue
@@ -33,7 +33,7 @@ import { U, token } from '../plugins/state.js'
 import { useRouter } from 'vue-router'
 import axios from '../plugins/axios'
 const router = useRouter()
-const start = new Date("2021-11-02T12:20:00.000+08:00").getTime()
+const start = new Date("2021-11-04T12:20:00.000+08:00").getTime()
 
 if (!U.value) router.push('/')
 
@@ -53,7 +53,7 @@ let clock = $ref('Loading')
 async function getAll() {
   pageLoading = true
   all = []
-  const findModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJcIixcIl9cIjpcIj9cIn0se1wiI1wiOlwiY2x1YmRhdGFcIixcIl9cIjpcIj9cIixcIjpcIjp7XCIke2lkfVwiOjF9fV0iLCI_IjpbImlkIl19.RAVszZa5qAaYdGQMzeQ0yLdmVdf7bVa_75k1wcYkGqAYoUp-qXhkhYz2lV8IqUs53rA1LPTPgnozDd9HwDI2zW6USVwV-qIparrcDk0hI2HsBN8vWpW0Ys1VBuYhHPTwhsyy0KQV-QWOfMarRVU1IXNH979dq0oXsefTmUSjDbNoKKOjjOJGYPwDbmhn4sZ2j069hqhgxlLIsMBBCE9azOLaTpayghRfKpY8p15E4YzmXP2Gcow28AIG2qnOof9komwVVHYm0sJf4CjkDvq4SbKU8tzob7Tj53_-uCz3wpv6eiOYDEKgtlrycUZ62gM2CzSoNZFC2pwXxXPKsIrPOg', s: { id: U.value.id } }
+  const findModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJcIixcIl9cIjpcIj9cIn0se1wiI1wiOlwiY2x1YmRhdGFcIixcIl9cIjpcIj9cIixcIjpcIjp7XCIke2lkfVwiOjF9fV0iLCI_IjpbImlkIl0sIm5iZiI6MTYzNTk5OTYwMDAwMH0.VsKedpKnSKRbfGIslUj4SHZYEjK253R7LxSR1cCcUJxcLJIVuRaihTCjYghy8VNRFn6jSIkTfUi3RJa6H6xHhEd_F_V7-staEWHAmFEkUWcOl9xa8w4gUl9JSgz8mrN6e3-FoVsHj8khqet5dNsPJt3gG6ZQ38hg1MiBUbEuDaOGzVrYwDXObl55eTEH9yDglV5Xycy2XIRGKK_8_kaJRj-HMX0XD83HPk6rjnnjsNGEbpd5nJadNf9X0LuYRlGojIwEngZJnrYm2vC08v2tnnJbFY3qP1GoDxgBVO3QrSSH--PKTvG2blCv9WLEIMpcjTBKMG23vymJy4mrE5sddA', s: { id: U.value.id } }
   await axios.post('/xyz', findModel, token())
     .then(({data}) => {
       if (!data[0].ok) Swal.fire('错误', '请重新进入', 'error')
@@ -74,7 +74,7 @@ async function submit (c, i) {
   if (selected) return
   loading = true
   let cancel = false
-  const submitModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJkYXRhXCIsXCJfXCI6XCI_XCIsXCIhXCI6MCxcIjpcIjp7XCIke2lkfVwiOjF9fSx7XCIjXCI6XCJjbHViXCIsXCI6XCI6e1wiJCR7aX1cIjp7XCJfXCI6XCJERUNcIn19fSx7XCIjXCI6XCJjbHViZGF0YVwiLFwiOlwiOntcIiR7aWR9XCI6XCIke2l9XCJ9fV0iLCI_IjpbImlkIiwiaSJdfQ.xZRTGjCEuOO9AUHaAEq72WDrj1WT9y_jDBCoi_yxxiCBpqkRvuS-epr18Pkvx1YkQA_haq26hQ9nHix4AyOu2WySDezbGmXVBSLd9YgPNLiuIG2_AatjONPZwsVR1IhfqFFZsy3ETWoHKTRBgaETQVH8g2WpA-cEQgWUWXiOj-PvHgWT_-xhGugyxeBTHk0-LIthlVk_KqTKEjAcPImezO41et5h6HUFWmOq4pUiOThLs3P8cj8b2AK-zLgImyOt4ZxQ36nOhZw9AZRXREFEAnbp_DjfU1ctUSyMxh6hIz0S2DWkROrU5bi67sguz6PaQpZYzH4nlTMk5CJoA2kVuw', s: { id: U.value.id, i: String(i) } }
+  const submitModel = { v: 'template', ':': 'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyI6IjoiW3tcIiNcIjpcImNsdWJkYXRhXCIsXCJfXCI6XCI_XCIsXCIhXCI6MCxcIjpcIjp7XCIke2lkfVwiOjF9fSx7XCIjXCI6XCJjbHViXCIsXCI6XCI6e1wiJCR7aX1cIjp7XCJfXCI6XCJERUNcIn19fSx7XCIjXCI6XCJjbHViZGF0YVwiLFwiOlwiOntcIiR7aWR9XCI6XCIke2l9XCJ9fV0iLCI_IjpbImlkIiwiaSJdLCJuYmYiOjE2MzU5OTk2MDAwMDB9.nqIA_Zd9eFXj1_5GMpyz49-Zr2mMY3up9IpfKukYWez97aM5u09C2QwbY9Ek0m5ghLqIsITzb-Og2IF_qOuyc1uzMMDLnPoFbHkc3LO6E4BcbD7ABqQrLI0fDPDLFnVnaAl1w-QlNl019maoBVzv0bVr44zeq4U-3nDAJ_luanjR7Nuu86OZyJf4kQ7GExoooywpO3rWzwqYj4TSM9vT7NQBT0uQqYTUbzRMTaSH1IrNsKyUToYrKyU3Zt2wp_nYz6x5O0pWVRwqBcG9yIFKV0H5CRZeShRWA9X6hGmfyQ64Y-wElwgvwlDZuMdBVFytPCRz0M8L_9aqNcFKvJe3BQ', s: { id: U.value.id, i: String(i) } }
   await Swal.fire({
     title: '确认加入吗？',
     text: '您即将加入' + c.name,
